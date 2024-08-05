e909078abf763ac921021c095c6db1dda46cfaab
Gautham C Sudheer <csgautham091003@gmail.com>
2024-08-05 16:09:54 +0530

    Allowed Host Change

--- malayalam_parser_project/settings.py
+++ malayalam_parser_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&*cr7&r2dfekv==1rdjn#kbk&()#m42i^o17y6vo#)ba!@)1$+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
